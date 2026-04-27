0022f56441443e10311080f126844ea7dd490245
zylen-seoul <guangmdc0901@gmail.com>
2026-04-27 10:43:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -206,6 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
+  base: "./",
   plugins,
   resolve: {
     alias: {
